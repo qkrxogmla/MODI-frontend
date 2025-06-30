ec0b813191b6cb5113363b6414f39ed6965defca
qkrxogmla <suk05553@naver.com>
2025-07-01 01:11:04 +0900

    [feat]footer 완성

--- Modi-frontend/src/components/common/Footer.tsx
+++ Modi-frontend/src/components/common/Footer.tsx
@@ -1,47 +1,33 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import style from "./Footer.module.css";
-
-const FooterIcon: { [key: string]: string } = {
-  home: "/icons/home.svg",
-  search: "/icons/search.svg",
-  add: "/icons/add.svg",
-  map: "/icons/map.svg",
-  mypage: "/icons/mypage.svg",
-};
+import FooterIcon from "./instance/footer";
 
 const Footer = () => {
   const navigator = useNavigate();
+  const location = useLocation();
+
   return (
     <div className={style.footer_wrapper}>
       <div className={style.footer_container}>
-        {Object.values(FooterIcon).map((icon, index) => (
-          <img
-            key={index}
-            src={icon}
-            alt={icon}
-            onClick={() => {
-              switch (index) {
-                case 0:
-                  navigator("/home");
-                  break;
-                case 1:
-                  navigator("/search");
-                  break;
-                case 2:
-                  navigator("/diary");
-                  break;
-                case 3:
-                  navigator("/map");
-                  break;
-                case 4:
-                  navigator("/mypage");
-                  break;
-                default:
-                  break;
-              }
-            }}
-          />
-        ))}
+        {Object.values(FooterIcon).map((icon, index) => {
+          const isCurrent = location.pathname.includes(
+            icon === "add" ? "diary" : icon
+          );
+          const iconSrc = isCurrent
+            ? `/icons/clicked_${icon}.svg`
+            : `/icons/${icon}.svg`;
+          return (
+            <img
+              className={style.footer_button}
+              key={index}
+              src={iconSrc}
+              alt={icon}
+              onClick={() => {
+                navigator(`/${icon === "add" ? "diary" : icon}`);
+              }}
+            />
+          );
+        })}
       </div>
     </div>
   );
