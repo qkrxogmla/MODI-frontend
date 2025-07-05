64bbf44df759be72c5c7839f6f5e08d7d3357f1e
joshcho <sungho020426@naver.com>
2025-07-05 15:15:51 +0900

    feat: 프레임 컴포넌트 제작 및 프레임 이미지 추가

---
+++ Modi-frontend/src/components/common/frame/Frame.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Frame.module.css";
+
+const Frame = () => {
+  return (
+    <div className={styles.frame}>
+      <div className={styles["image-frame"]}>
+        <img
+          className={styles.image}
+          src="https://placehold.co/227x262"
+          alt="사진"
+        />
+        <span className={styles.date}>25.5.13 12:17</span>
+      </div>
+      <span className={styles.summary}>일기 내용 한 줄 요약</span>
+    </div>
+  );
+};
+
+export default Frame;
