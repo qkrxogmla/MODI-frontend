6c00b6212875151cb5712fbd6d8641eb7792e3c5
joshcho <sungho020426@naver.com>
2025-07-06 14:08:34 +0900

    feat: 로그인 화면 - 초기설정 화면 라우팅

--- Modi-frontend/src/pages/login/LoginPage.tsx
+++ Modi-frontend/src/pages/login/LoginPage.tsx
@@ -1,9 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./LoginPage.module.css";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const handleGoogleLogin = () => {
+    // 로그인 로직 여기서 구현!
+    navigate("/test-initialsetting");
+  };
+
   return (
     <div className={styles.loginPage}>
-      <button className={styles.googleLogin_btn}>
+      <button className={styles.googleLogin_btn} onClick={handleGoogleLogin}>
         <span>구글로 시작하기</span>
       </button>
     </div>
