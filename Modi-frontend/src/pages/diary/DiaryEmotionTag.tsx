4bfd82b1f7bcb6f348f38a8f131bd5a4a1dc60ea
qkrxogmla <suk05553@naver.com>
2025-07-08 21:56:57 +0900

    [feat] root에 폰트 및 색깔 변수 추가

--- Modi-frontend/src/pages/diary/DiaryEmotionTag.tsx
+++ Modi-frontend/src/pages/diary/DiaryEmotionTag.tsx
@@ -1,9 +1,16 @@
 import styles from "./DiaryEmotionTag.module.css";
+import Header from "../../components/common/Header";
 
 const DiaryEmotionTag = () => {
   return (
     <div className={styles.DiaryEmotionTag_wrapper}>
-      <div className={styles.DiaryEmotionTag_container}>DiaryEmotionTag</div>
+      <div className={styles.DiaryEmotionTag_container}>
+        <Header />
+        <div className={styles.main_container}>
+          <p className={styles.ask}>오늘은 어떤 하루였나요?</p>
+          DiaryEmotionTag
+        </div>
+      </div>
     </div>
   );
 };
