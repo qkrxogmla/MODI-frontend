f179b27e46c6f58e5dba621c2b4b87a434d2efe8
unknown <egg9054@gmail.com>
2025-07-09 17:34:20 +0900

    [Fix] 배포 오류 코드 수정 및 임시 주석 처리

--- Modi-frontend/src/components/diary/PolaroidDiary.tsx
+++ Modi-frontend/src/components/diary/PolaroidDiary.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Frame from "../common/frame/Frame";
-import TagChip from "../tag/EmotionTag";
+import TagChip from "../tag/EmotionTag/EmotionTag";
 {/* 캐릭터가 없음 */ }
-import { DiaryProps } from "./Diary";
 import styles from "./PolaroidDiary.module.css";
 
 
-export interface PolaroidDiaryProps extends DiaryProps {
+export interface PolaroidDiaryProps {
     date: string;
     photoUrl: string;
     emotion: string;
@@ -26,21 +25,23 @@ const PolaroidDiary: React.FC<PolaroidDiaryProps> = ({
     characterId,
 }) => (
     <div className={styles.container}>
-        <Frame
+        {/*   <Frame
             photoUrl={photoUrl}
             date={date}
             summary={content}
             className={styles.frame}
         />
         <div className={styles.info}>
-            <Character emotion={emotion} characterId={selectedCharacterId} />
-            <div className={styles.tags}>
-                {tags.map((tag, idx) => (
-                    <TagChip key={idx} label={tag} />
-                ))}
-            </div>
+            <Character emotion={emotion} characterId={characterId} />
+            */}
+        <div className={styles.tags}>
+            {tags.map((tag, idx) => (
+                <TagChip key={idx} label={tag} type="momo"/>
+                {/*EmotionTagList가 완성되지 않아 임시로 캐릭터 타입은 momo로 설정*/}
+            ))}
         </div>
     </div>
+
 );
 
 export default PolaroidDiary;
