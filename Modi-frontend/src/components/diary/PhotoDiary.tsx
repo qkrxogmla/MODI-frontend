0724da63a1b985978ea910539c65f1d98ad61ddb
unknown <egg9054@gmail.com>
2025-07-09 12:07:54 +0900

    [feat] 홈화면 Diary, PolaroidDiary, PhotoDiary 추가

--- Modi-frontend/src/components/diary/PhotoDiary.tsx
+++ Modi-frontend/src/components/diary/PhotoDiary.tsx
@@ -1,3 +1,11 @@
-// import React from "react";
-// import Diary from "./Diary";
-// import styles from "./PhotoDiary.module.css";
+import React from "react";
+import Diary, { DiaryProps } from "./Diary";
+import styles from "./PhotoDiary.module.css";
+
+const PhotoDiary: React.FC<DiaryProps> = ({ emotion, content, clicked }) => (
+  <div className={styles.card}>
+    <Diary emotion={emotion} content={content} clicked={clicked} />
+  </div>
+);
+
+export default PhotoDiary;
--- Modi-frontend/src/components/diary/PolaroidDiary.tsx
+++ Modi-frontend/src/components/diary/PolaroidDiary.tsx
@@ -1,3 +1,42 @@
-// import React from "react";
-// import Diary from "./Diary";
-// import styles from "./PolaroidDiary.module.css";
+import React from "react";
+import Frame from "../common/frame/Frame";
+import TagChip from "../tag/EmotionTag";
+{/* 캐릭터가 없음 */}
+import { DiaryProps } from "./Diary";
+import styles from "./PolaroidDiary.module.css";
+
+
+export interface PolaroidDiaryProps extends DiaryProps {
+  date: string;
+  photoUrl: string;
+  tags?: string[];
+}
+
+const PolaroidDiary: React.FC<PolaroidDiaryProps> = ({
+  date,
+  photoUrl,
+  emotion,
+  content,
+  clicked,
+  tags = [],
+}) => (
+  <div className={styles.container}>
+    <Frame
+      photoUrl={photoUrl}
+      date={date}
+      summary={content}
+      className={styles.frame}
+    />
+    <div className={styles.info}>
+      <div className={styles.tags}>
+        {tags.map((tag, idx) => (
+          <TagChip key={idx} label={tag} />
+        ))}
+      </div>
+      <Character emotion={emotion} characterId={selectedCharacterId} />
+      <Diary emotion={emotion} content={content} clicked={clicked} />
+    </div>
+  </div>
+);
+
+export default PolaroidDiary;
