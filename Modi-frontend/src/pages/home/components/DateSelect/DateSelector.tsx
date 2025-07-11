39bb3474c6f9f7d138ec06fc5b57767848f66411
Jung-Eon <egg9054@gmail.com>
2025-07-11 16:57:05 +0900

    [feat]DateSelector 날짜 선택바 색상 캐릭터 타입으로 부터 받기

--- Modi-frontend/src/pages/home/components/DateSelect/DateSelector.tsx
+++ Modi-frontend/src/pages/home/components/DateSelect/DateSelector.tsx
@@ -1,5 +1,9 @@
 import React, { useState, useMemo, useRef, useEffect } from "react";
 import styles from "./DateSelector.module.css";
+import {
+  CharacterType,
+  useCharacter,
+} from "../../../../contexts/CharacterContext";
 
 export interface DiaryItem {
   date: string;
@@ -10,13 +14,22 @@ interface Props {
   items: DiaryItem[]; //모든 일기들의 날짜 데이터
   initialDate: string; //처음에 선택되어 있어야 할 날짜
   onChange: (date: string) => void;
+  userCharacter: CharacterType;
 }
+
+const colorMap: Record<NonNullable<CharacterType>, string> = {
+  momo: "#fbd7d5",
+  boro: "#FEE888",
+  lumi: "#A7E1B6",
+  zuni: "#93D1E0",
+};
 
 const DateSelector: React.FC<Props> = ({
   viewType,
   items,
   initialDate,
   onChange,
+  userCharacter,
 }) => {
   const [year, setYear] = useState(initialDate.slice(0, 4));
   const [month, setMonth] = useState(initialDate.slice(5, 7));
@@ -92,6 +105,12 @@ const DateSelector: React.FC<Props> = ({
     if (viewType === "polaroid") onChange(`${year}-${month}-${day}`);
     else onChange(`${year}-${month}`);
   }, [year, month, day]);
+
+  const highlight = userCharacter ? colorMap[userCharacter] : "#ccc";
+
+  const styleVars = {
+    "--picker-highlight": highlight,
+  } as React.CSSProperties;
 
   return (
     <div className={styles.picker}>
