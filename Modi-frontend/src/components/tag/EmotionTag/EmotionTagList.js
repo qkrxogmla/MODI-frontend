b95170368f780f567edf5fdfebab0f3e6e2cd6e9
Jung-Eon <egg9054@gmail.com>
2025-07-14 23:11:19 +0900

    Feat:폴라로이드 다이어리 하단 EmotionTagList 컴포넌트 추가

--- Modi-frontend/src/components/tag/EmotionTag/EmotionTagList.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-// import EmotionTag from "./EmotionTag";
-export default function EmotionTagList() {
-    return (_jsx("div", {}));
-}
--- Modi-frontend/src/components/tag/EmotionTag/EmotionTagList.tsx
+++
@@ -1,9 +0,0 @@
-// import EmotionTag from "./EmotionTag";
-
-export default function EmotionTagList() {
-  return (
-    <div>
-      {/* 나중에 여기에 사용자에게 입력받은 EmotionTag들이 들어갈 예정 */}
-    </div>
-  );
-}
