03f378dc6df5c9eb8925ada0fe041bf98e47265a
joshcho <sungho020426@naver.com>
2025-07-14 21:36:50 +0900

    feat: RecordDetailPage.tsx 버튼 공통 컴포넌트로 교체

--- Modi-frontend/src/components/common/button/ButtonIcon/FavoriteButton.tsx
+++ Modi-frontend/src/components/common/button/ButtonIcon/FavoriteButton.tsx
@@ -2,13 +2,14 @@ import styles from "./ButtonIcon.module.css";
 
 interface ButtonIconProps {
   onClick?: () => void;
+  isFavorite?: boolean;
 }
 
-export default function ButtonIcon({ onClick }: ButtonIconProps) {
+export default function ButtonIcon({ onClick, isFavorite }: ButtonIconProps) {
   return (
     <button className={styles.container} onClick={onClick}>
       <img
-        src="/icons/favorite.svg"
+        src={isFavorite ? "/icons/favorite_on.svg" : "/icons/favorite_off.svg"}
         alt="즐겨찾기 아이콘"
         className={styles.icon}
       />
