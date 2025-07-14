b3ab7ce3b573c76e79e24d4b3f9aedd05222008f
Jung-Eon <egg9054@gmail.com>
2025-07-14 21:56:59 +0900

    Feat(utils):캐릭터 감정 별 SVG 자동 매핑 유틸 추가

---
+++ Modi-frontend/src/utils/emotionImages.ts
@@ -0,0 +1,38 @@
+export type CharacterType = "momo" | "boro" | "lumi" | "zuni";
+export type Emotion =
+  | "기쁨"
+  | "놀람"
+  | "보통"
+  | "떨림"
+  | "사랑"
+  | "신남"
+  | "아픔"
+  | "슬픔"
+  | "지루함"
+  | "화남";
+
+interface IconModule {
+  ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+}
+
+const modules = import.meta.globEager<IconModule>(
+  "../public/emotion_home/**/*.svg",
+  { import: "ReactComponent" }
+);
+
+export const emotionIconMap: Record<
+  CharacterType,
+  Record<Emotion, React.FC<React.SVGProps<SVGSVGElement>>>
+> = {} as any;
+
+Object.entries(modules).forEach(([path, mod]) => {
+  const parts = path.split("/");
+  const character = parts[parts.length - 2] as CharacterType;
+  const fileName = parts[parts.length - 1]; // 'home_momo-happy.svg'
+  const [, emotion] = fileName.match(/home_[^-]+-([^.]+)\.svg/)!;
+
+  if (!emotionIconMap[character]) {
+    emotionIconMap[character] = {} as any;
+  }
+  emotionIconMap[character][emotion as Emotion] = mod.ReactComponent;
+});
