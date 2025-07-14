fc3c5d0c052359144a21631f4e8b919053802c84
Jung-Eon <egg9054@gmail.com>
2025-07-14 20:57:19 +0900

    Feat:mock 데이터에 속성 추가

--- Modi-frontend/src/data/diaries.ts
+++ Modi-frontend/src/data/diaries.ts
@@ -1,17 +1,47 @@
-import { Emotion } from "../components/HomePage/EmotionTab/EmotionTab";
+export type Emotion =
+  | "기쁨"
+  | "놀람"
+  | "보통"
+  | "떨림"
+  | "사랑"
+  | "신남"
+  | "아픔"
+  | "슬픔"
+  | "지루함"
+  | "화남";
 
 export interface Diary {
   id: number;
   date: string; // "YYYY-MM-DD"
   photoUrl: string;
-  emotion: Emotion; // "기쁨" | "슬픔" | …
+  summary: string;
+  emotion: Emotion;
+  tags?: string[];
 }
 
 // 임시 더미 데이터
 export const allDiaries: Diary[] = [
-  { id: 1, date: "2025-07-10", photoUrl: "/img/1.jpg", emotion: "기쁨" },
-  { id: 2, date: "2025-07-10", photoUrl: "/img/2.jpg", emotion: "슬픔" },
-  { id: 3, date: "2025-06-15", photoUrl: "/img/3.jpg", emotion: "보통" },
+  {
+    id: 1,
+    date: "2025-07-10",
+    photoUrl: "/img/1.jpg",
+    summary: "",
+    emotion: "기쁨",
+  },
+  {
+    id: 2,
+    date: "2025-07-10",
+    photoUrl: "/img/2.jpg",
+    summary: "",
+    emotion: "슬픔",
+  },
+  {
+    id: 3,
+    date: "2025-06-15",
+    photoUrl: "/img/3.jpg",
+    summary: "",
+    emotion: "보통",
+  },
   // …필요한 만큼 추가
 ];
 
