73291df693d4961a74b6f73c45df37ecfbaedb3a
Jung-Eon <egg9054@gmail.com>
2025-07-20 17:54:17 +0900

    Feat:Header추가

--- Modi-frontend/src/pages/home/HomePage.tsx
+++ Modi-frontend/src/pages/home/HomePage.tsx
@@ -5,10 +5,13 @@ import Footer from "../../components/common/Footer";
 import PolaroidView from "./PolaroidView";
 import PhotoView from "./PhotoView";
 
+import Header from "../../components/common/Header";
+
 export default function HomePage() {
   const [viewType, setViewType] = useState<"photo" | "polaroid">("polaroid");
   return (
     <div className={style.home_wrapper}>
+      <Header left="/images/logo/Modi.svg" right="/icons/notification_O.svg" />
       <div className={style.home_container}>
         <main className={style.mainContent}>
           {viewType === "photo" ? (
