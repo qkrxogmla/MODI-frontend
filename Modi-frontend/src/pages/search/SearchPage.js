f5436556e45dd7e518ae1ad6c21b7d87085acfa7
qkrxogmla <suk05553@naver.com>
2025-07-22 17:41:52 +0900

    일기 검색하기 틀 추가

--- Modi-frontend/src/pages/search/SearchPage.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const SearchPage = () => {
-    return _jsx("div", { children: "SearchPage" });
-};
-export default SearchPage;
--- Modi-frontend/src/pages/search/SearchPage.tsx
+++ Modi-frontend/src/pages/search/SearchPage.tsx
@@ -1,5 +1,15 @@
+import styles from "./SearchPage.module.css";
+import Header from "../../components/common/Header";
+
 const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <div className={styles.SearchPage_wrapper}>
+      <div className={styles.SearchPage_container}>
+        <Header middle="일기 검색하기" />
+        <div className={styles.main_container}></div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchPage;
