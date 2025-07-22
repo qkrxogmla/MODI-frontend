8633e1fa0b7d6f6004bb2c263fcb0839ce1856c6
qkrxogmla <suk05553@naver.com>
2025-07-22 18:13:07 +0900

    키워드 부분과  헤더에서 팝업생기는 조건 후에 수정 예정

--- Modi-frontend/src/pages/search/SearchPage.tsx
+++ Modi-frontend/src/pages/search/SearchPage.tsx
@@ -6,7 +6,21 @@ const SearchPage = () => {
     <div className={styles.SearchPage_wrapper}>
       <div className={styles.SearchPage_container}>
         <Header middle="일기 검색하기" />
-        <div className={styles.main_container}></div>
+        <div className={styles.main_container}>
+          <div className={styles.search_container}>
+            <input
+              type="text"
+              placeholder="키워드를 입력해주세요"
+              className={styles.search_input}
+            />
+            <img
+              className={styles.search_icon}
+              src="/icons/black_search.svg"
+              alt="검색"
+              style={{ cursor: "pointer" }}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
