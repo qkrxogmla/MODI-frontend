5ea0900f3919c241cde91b39067913a24e51d856
Jung-Eon <egg9054@gmail.com>
2025-07-23 00:25:29 +0900

    Feat:마이페이지 렌더링

--- Modi-frontend/src/pages/mypage/MyPage.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const MyPage = () => {
-    return _jsx("div", { children: "MyPage" });
-};
-export default MyPage;
--- Modi-frontend/src/pages/mypage/MyPage.tsx
+++ Modi-frontend/src/pages/mypage/MyPage.tsx
@@ -1,5 +1,18 @@
+import style from "./MyPage.module.css";
+import Header from "../../components/common/Header";
+
 const MyPage = () => {
-  return <div>MyPage</div>;
+  return (
+    <div className={style.mypage_wrapper}>
+      <div className={style.mypage_container}>
+        <Header
+          left="/icons/setting.svg"
+          middle="마이페이지"
+          right="/icons/notification_O.svg"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default MyPage;
