ce4aa8f111ea62fb2cff2f61e132af78cc806a7e
Jung-Eon <egg9054@gmail.com>
2025-07-23 18:21:44 +0900

    Feat:즐겨찾기 그리드 컴포넌트 추가

--- Modi-frontend/src/pages/mypage/MyPage.tsx
+++ Modi-frontend/src/pages/mypage/MyPage.tsx
@@ -4,8 +4,13 @@ import Header from "../../components/common/Header";
 import ProfileCard from "../../components/MyPage/Profile/ProfileCard";
 import TabBar from "../../components/MyPage/TabBar";
 import Footer from "../../components/common/Footer";
+import FavoriteDiary, {
+  FavoriteDiaryProps,
+} from "../../components/MyPage/Favorite/FavoriteDiary";
+import { allDiaries, Diary } from "../../data/diaries";
 
 const MyPage = () => {
+  const filtered: Diary[] = allDiaries;
   return (
     <div className={style.mypage_wrapper}>
       <div className={style.mypage_container}>
@@ -19,6 +24,18 @@ const MyPage = () => {
         </div>
         <div className={style.tab_bar}>
           <TabBar />
+        </div>
+        <div className={style.photoGrid}>
+          {filtered.map((d) => (
+            <FavoriteDiary
+              key={d.id}
+              id={d.id}
+              photoUrl={d.photoUrl}
+              date={d.date}
+              emotion={d.emotion}
+              clicked={false}
+            />
+          ))}
         </div>
         <Footer />
       </div>
