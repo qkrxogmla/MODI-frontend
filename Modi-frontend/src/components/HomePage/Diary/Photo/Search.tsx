02733d75c77965b5a832235a283a77b1b495b3e3
Jung-Eon <egg9054@gmail.com>
2025-07-23 16:53:15 +0900

    Feat:선택한 감정 일기 없을 때 뜨는 캐릭터 컴포넌트 추가

---
+++ Modi-frontend/src/components/HomePage/Diary/Photo/Search.tsx
@@ -0,0 +1,40 @@
+import React, { useContext } from "react";
+import {
+  useCharacter,
+  CharacterType,
+} from "../../../../contexts/CharacterContext";
+import style from "./Search.module.css";
+
+import momoSvg from "images/search-character/search-momo.svg";
+import boroSvg from "images/search-character/search-boro.svg";
+import lumiSvg from "images/search-character/search-lumi.svg";
+import zuniSvg from "images/search-character/search-zuni.svg";
+import defaultSvg from "images/search-character/search-momo.svg";
+
+type Character = "momo" | "boro" | "lumi" | "zuni" | null;
+
+const svgMap = new Map<Character, string>([
+  ["momo", momoSvg],
+  ["boro", boroSvg],
+  ["lumi", lumiSvg],
+  ["zuni", zuniSvg],
+  [null, defaultSvg],
+]);
+
+export default function CharacterImage() {
+  const { character } = useCharacter();
+  if (!character) return null;
+
+  const src = svgMap.get(character) ?? defaultSvg;
+
+  return (
+    <div className={style.searchCharacterContainer}>
+      <img
+        src={src}
+        alt={character ? `${character} 아이콘` : "기본 아이콘"}
+        className={style.searchCharacterImage}
+      />
+      <span className={style.text}>작성한 기록이 없습니다</span>
+    </div>
+  );
+}
--- Modi-frontend/src/components/HomePage/Diary/Polaroid/EmotionCharacter.tsx
+++ Modi-frontend/src/components/HomePage/Diary/Polaroid/EmotionCharacter.tsx
@@ -9,11 +9,9 @@ interface Props {
 
 export default function EmotionCharacter({ emotion }: Props) {
   const { character } = useCharacter();
-  console.log("▶ EmotionCharacter:", { character, emotion });
   if (!character) return null; // 아직 선택 안 된 경우
   // 이 시점에 character 는 CharacterKey 와 동일
   const src = getEmotionIcon(character, emotion);
-  console.log("▶ getEmotionIcon 반환:", src);
   if (!src) return null;
   return <img src={src} alt={emotion} />;
 }
